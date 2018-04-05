9e4e3dc0e7182c23fafdfcca334c28cdee6849fb
TZ <atian25@qq.com>
2018-04-05 13:54:11 +0800

    fix: test

--- boilerplate/test/app/service/Test.test.ts
+++ boilerplate/test/app/service/Test.test.ts
@@ -1,7 +1,8 @@
 'use strict';
 
+import * as assert from 'assert';
 import { Context } from 'egg';
-import { app, assert } from 'egg-mock/bootstrap';
+import { app } from 'egg-mock/bootstrap';
 
 describe('test/app/service/Test.test.js', () => {
   let ctx: Context;
