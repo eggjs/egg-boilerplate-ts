6f8ad90f1b42c55bc454bf0bf7210f8227e40ed3
wanghx <whx89768@alibaba-inc.com>
2018-09-11 10:53:49 +0800

    feat: update deps and code optimization

--- boilerplate/config/config.default.ts
+++ boilerplate/config/config.default.ts
@@ -1,25 +1,23 @@
 import { EggAppConfig, EggAppInfo, PowerPartial } from 'egg';
 
-// for config.{env}.ts
-export type DefaultConfig = PowerPartial<EggAppConfig & BizConfig>;
-
-// app special config scheme
-export interface BizConfig {
-  sourceUrl: string;
-}
-
 export default (appInfo: EggAppInfo) => {
-  const config = {} as PowerPartial<EggAppConfig> & BizConfig;
-
-  // app special config
-  config.sourceUrl = `https://github.com/eggjs/examples/tree/master/${appInfo.name}`;
+  const config = {} as PowerPartial<EggAppConfig>;
 
   // override config from framework / plugin
   // use for cookie sign key, should change to your own and keep security
   config.keys = appInfo.name + '_{{keys}}';
 
-  // add your config here
+  // add your egg config in here
   config.middleware = [];
 
-  return config;
+  // add your special config in here
+  const bizConfig = {
+    sourceUrl: `https://github.com/eggjs/examples/tree/master/${appInfo.name}`,
+  };
+
+  // the return config will combines to EggAppConfig
+  return {
+    ...config,
+    ...bizConfig,
+  };
 };
--- boilerplate/typings/index.d.ts
+++ boilerplate/typings/index.d.ts
@@ -1,3 +1,5 @@
+import 'egg';
+
 declare module 'egg' {
 
 }
