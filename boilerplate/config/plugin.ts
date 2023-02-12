0469484db59c215db7fb3ea1856416a6726bd32f
fengmk2 <fengmk2@gmail.com>
2023-02-12 15:30:23 +0800

    feat: Upgrade to tegg by default

--- boilerplate/config/plugin.ts
+++ boilerplate/config/plugin.ts
@@ -1,11 +1,34 @@
 import { EggPlugin } from 'egg';
 
 const plugin: EggPlugin = {
-  // static: true,
-  // nunjucks: {
-  //   enable: true,
-  //   package: 'egg-view-nunjucks',
-  // },
+  tegg: {
+    enable: true,
+    package: '@eggjs/tegg-plugin',
+  },
+  teggConfig: {
+    enable: true,
+    package: '@eggjs/tegg-config',
+  },
+  teggController: {
+    enable: true,
+    package: '@eggjs/tegg-controller-plugin',
+  },
+  teggSchedule: {
+    enable: true,
+    package: '@eggjs/tegg-schedule-plugin',
+  },
+  eventbusModule: {
+    enable: true,
+    package: '@eggjs/tegg-eventbus-plugin',
+  },
+  aopModule: {
+    enable: true,
+    package: '@eggjs/tegg-aop-plugin',
+  },
+  tracer: {
+    enable: true,
+    package: 'egg-tracer',
+  },
 };
 
 export default plugin;
